decd5db1d6411ad6f8766aaa9f02594aead24df7
Vistril69 <scamomilegm@gmail.com>
2026-08-07 21:59:13 -0600

    skeleton for sound playback

--- src/audio/Snd_Main.cpp
+++ src/audio/Snd_Main.cpp
@@ -7,6 +7,10 @@
 static ALCdevice    *snd_device;
 static ALCcontext   *snd_context;
 
+// Test buffer and source for audio playback testing
+static ALuint       snd_testBuffer;
+static ALuint       snd_testSource;
+
 void Snd_Init( void )
 {
     snd_device = alcOpenDevice( NULL );
@@ -44,4 +48,45 @@ void Snd_Shutdown( void )
     }
 
     Com_Printf( SND_LOG "Shutdown OpenAL audio system\n" );
+}
+
+static ALenum Snd_ALFormat( int channels ) 
+{
+// TODO: return AL_FORMAT_MONO16 or AL_FORMAT_STEREO16 based on channels
+//      if channels is neither 1 or 2, log a warning and return 0    
+}
+
+static void Cmd_PlaySnd( void )
+{
+    if ( Cmd_Argc() < 2 )
+    {
+        Com_Printf( SND_LOG "Usage: playsnd <filepath>\n" );
+        return;
+    }
+
+    // TODO if a previous sound is still loaded, clean it up:
+    // alDeleteSources( 1, &snd_testSource );
+    // alDeleteBuffers( 1, &snd_testBuffer );
+    // zero both handles
+
+    // TODO Snd_Decode( Cmd_Argv( 1 ) ) in to a sndPcm_t 
+    // exit on failure
+
+    // TODO: figure out the AL format with Snd_ALFormat
+    // exit if 0
+
+    // TODO: Calculate data size in bytes 
+    //       frames * channels * sizeof( drflac_int16 )
+
+    // TODO: alGenBuffers( 1, &snd_testBuffer )
+    //       alBufferData( snd_testBuffer, format, pcm.data, dataSize, pcm.rate )
+    
+    // TODO: Snd_FreePcm: OpenAL copied the data, we don't need ours anymore
+
+    // TODO: alGenSources( 1, &snd_testSource )
+    //       alSourcei( snd_testSource, AL_BUFFER, snd_testBuffer )
+    //       alSourcePlay( snd_testSource )
+
+    // TODO: print what's currently playing 
+    
 }
